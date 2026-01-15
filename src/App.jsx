c009a198328ab2026c196f2437a1d3d19deffd43
Abdelrhman <adodoadodo2025@gmail.com>
2026-01-16 00:49:59 +0200

    Add delete key functionality to remove the last note and manage event listeners

--- src/App.jsx
+++ src/App.jsx
@@ -40,8 +40,20 @@ export default function App() {
       addNote(inputValue);
     }
   }
+  function deleteKeyNotes(e) {
+    if (e.key === "Delete" && notes.length > 0) {
+      deleteNote(notes[notes.length - 1].id);
+    }
+  }
   useEffect(() => {
     localStorage.setItem("notes", JSON.stringify(notes));
+  }, [notes]);
+
+  useEffect(() => {
+    window.addEventListener("keydown", deleteKeyNotes);
+    return () => {
+      window.removeEventListener("keydown", deleteKeyNotes);
+    };
   }, [notes]);
   return (
     <main
@@ -63,9 +75,9 @@ export default function App() {
           viewBox="0 0 24 24"
         >
           <path
-            fill-rule="evenodd"
+            fillRule="evenodd"
             d="M11.675 2.015a.998.998 0 0 0-.403.011C6.09 2.4 2 6.722 2 12c0 5.523 4.477 10 10 10 4.356 0 8.058-2.784 9.43-6.667a1 1 0 0 0-1.02-1.33c-.08.006-.105.005-.127.005h-.001l-.028-.002A5.227 5.227 0 0 0 20 14a8 8 0 0 1-8-8c0-.952.121-1.752.404-2.558a.996.996 0 0 0 .096-.428V3a1 1 0 0 0-.825-.985Z"
-            clip-rule="evenodd"
+            clipRule="evenodd"
           />
         </svg>
       </button>
